86828005174274cb2a85cab0e4997c84d7f1e6f5
Bidna <daria.wiseau@gmail.com>
2020-05-25 22:44:16 +0300

    booster
    booster and heal item are ready

---
+++ Assets/Scripts/Boost.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boost : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Player player = FindObjectOfType<Player>();
+        player.StartCoroutine("boostTimer");
+        Destroy(this.gameObject);
+    }
+    
+}
