c4412ca5078f6d117cd0ffccedd8f42b71818e0a
Bidna <daria.wiseau@gmail.com>
2020-05-26 13:20:23 +0300

    new main hero
    changed sprites of the main character and fixed a bug when player try to pick up boost while already beeing boosted

--- Assets/Scripts/Boost.cs
+++ Assets/Scripts/Boost.cs
@@ -7,8 +7,11 @@ public class Boost : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collider)
     {
         Player player = FindObjectOfType<Player>();
-        player.StartCoroutine("boostTimer");
-        Destroy(this.gameObject);
+        if(player.speed == 4)
+        {
+            player.StartCoroutine("boostTimer");
+            Destroy(this.gameObject);
+        }
     }
     
 }
