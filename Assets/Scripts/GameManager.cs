83fbbd23229f3b1439c9bb212bf26c1fe9ecf329
Nikita Brik <keltis.kk@gmail.com>
2020-06-03 22:50:14 +0300

    DoubleMindsWin 1.0.0.8

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,15 @@ public class GameManager : MonoBehaviour
     public static event Action<bool> OnWorldChange ;
     private bool normalWorld = true;
     public bool NormalWorld => normalWorld;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            ChangeWorld();
+        }
+    }
+
     public void ChangeWorld()
     {
         normalWorld = !normalWorld;
